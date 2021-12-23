e2b1737458acb98c9cdacec6336a324b6643df40
Gergő <mogema2002@gmail.com>
2021-12-23 10:31:01 +0100

    login

--- src/konyvtar/Konyvtar.java
+++
@@ -1,9 +0,0 @@
-package konyvtar;
-
-public class Konyvtar {
-
-    public static void main(String[] args) {
-        new Gui().setVisible(true);
-    }
-    
-}
---
+++ src/konyvtar/User.java
@@ -0,0 +1,10 @@
+package konyvtar;
+public class User {
+    private String username,password;
+    private boolean login=false;
+    
+    
+    public boolean loginValue(){
+        return login;
+    }
+}
