279c88ceaf392bc92ac0b208c27e41a39cfadb33
Gergő <mogema2002@gmail.com>
2021-12-23 11:18:04 +0100

    Database
    Database connection from older code

---
+++ src/konyvtar/dbConnect.java
@@ -0,0 +1,54 @@
+package konyvtar;
+
+import java.sql.*;
+public class dbConnect {
+    private Connection myConn;
+    public dbConnect(){
+        try{
+            myConn= DriverManager.getConnection("jdbc:mysql://localhost:3306/konyvtar","phpAdmin","5mtYrEd0j1nWomRb");
+        }catch(Exception e){
+            System.err.println(e.getMessage());
+        }
+    }
+    
+    public ResultSet getResult(String sql){
+        try{
+            Statement myStmt=this.myConn.createStatement();
+            ResultSet myRs = myStmt.executeQuery(sql);
+            return myRs;
+        }
+        catch(Exception e){
+            System.err.println(e.getMessage());
+        }
+        return null;
+    }
+    
+    public boolean insertToSql(String table,String value){
+        //table: "name(rownames)" example: "users(username,password)"
+        //value: "Values(values)" example: "VALUES ('username','password')"
+        if(table.equals("") || value.equals("")){
+            return false;
+        }
+        try{
+            Statement myStmt=this.myConn.createStatement();
+            myStmt.executeUpdate("INSERT INTO "+table +" " +value);
+            return true;
+        }
+        catch(Exception e){
+            System.err.println(e.getMessage());
+        }
+        return false;
+    }
+    
+    public boolean deleteFromSql(String statement){
+        try{
+            String query = "Delete From "+statement; 
+            PreparedStatement preparedStmt = this.myConn.prepareStatement(query);
+            preparedStmt.execute();
+            return true;
+        }catch (Exception e){
+            System.err.println(e.getMessage());
+        }
+        return false;
+    }
+}
