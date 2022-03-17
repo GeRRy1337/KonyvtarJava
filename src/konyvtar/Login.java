f69d871b6b0ada37d82d77fdfc955d22e768ee96
GeRRy1337 <mogema2002@gmail.com>
2022-03-17 08:30:05 +0100

    bug fix

--- src/konyvtar/Login.java
+++ src/konyvtar/Login.java
@@ -96,10 +96,10 @@ public class Login extends javax.swing.JFrame {
 
         jLabel2.setText("Jelszó:");
 
-        login_icon.setIcon(new javax.swing.ImageIcon("D:\\Dokumentumok\\NetBeansProjects\\Konyvtar\\lib\\login_icon.png")); // NOI18N
+        login_icon.setIcon(new javax.swing.ImageIcon("C:\\Users\\Molnár Gergő\\Documents\\NetBeansProjects\\KonyvtarJava\\lib\\login_icon.png")); // NOI18N
 
         settings.setBackground(new java.awt.Color(255, 255, 255));
-        settings.setIcon(new javax.swing.ImageIcon("D:\\Dokumentumok\\NetBeansProjects\\Konyvtar\\lib\\settings_icon.png")); // NOI18N
+        settings.setIcon(new javax.swing.ImageIcon("C:\\Users\\Molnár Gergő\\Documents\\NetBeansProjects\\KonyvtarJava\\lib\\settings_icon.png")); // NOI18N
         settings.setAutoscrolls(true);
         settings.setBorder(null);
         settings.setBorderPainted(false);
@@ -149,7 +149,7 @@ public class Login extends javax.swing.JFrame {
                 .addComponent(pass, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addComponent(submit)
-                .addContainerGap(83, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
